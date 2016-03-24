5451b72be0f309905f4448ab09782d22c9f9bc25
Rafael Silva Guimarães <rafaelg@ifes.edu.br>
2016-03-24 02:40:50 -0300

    Questao04 - Comentários

--- TP2016_1/Exercícios/Lista03/questao04.c
+++ TP2016_1/Exercícios/Lista03/questao04.c
@@ -52,14 +52,16 @@ int menu(){
 int verificarLinhas(char m[][COL], char jogador){
     int i,j;
     int cont=0;
-
     for(i=0;i<LIN;i++){
         cont = 0;
         for(j=0;j<COL;j++){
+            // verifica se o valor eh igual do jogador
             if(m[i][j] == jogador){
                 cont++;
             }
         }
+        // Se cada linha a contagem for igual a 3
+        // O jogador venceu
         if(cont == 3){
             system("clear");
             printf("\t\tJogador %c VENCEU!!\n", jogador);
@@ -76,10 +78,15 @@ int verificarColunas(char m[][COL], char jogador){
     for(i=0;i<LIN;i++){
         cont = 0;
         for(j=0;j<COL;j++){
+            // Verifica ahora as colunas alterando i e j
+            // que determina o modo de interacao emtre os
+            // elementos
             if(m[j][i] == jogador){
                 cont++;
             }
         }
+        // Se cada linha a contagem for igual a 3
+        // O jogador venceu
         if(cont == 3){
             system("clear");
             printf("\t\tJogador %c VENCEU!!\n", jogador);
@@ -93,24 +100,33 @@ int verificarDiagonais(char m[][COL], char jogador){
     int i,j;
     int cont=0;
 
-    // Diagonal Principal
+    // Verifica usando a diagonal principal
     for(i=0;i<LIN;i++){
+        // Para isso, utiliza linha=i e coluna=i
         if(m[i][i] == jogador){
             cont++;
         }
     }
+    // Se cada linha a contagem for igual a 3
+    // O jogador venceu pela diagonal principal
     if(cont == 3){
         system("clear");
         printf("\t\tJogador %c VENCEU!!\n", jogador);
         system("sleep 2");
         return 1;
     }else{
+        // Se nao conseguiu detectar nenhum jogo pela
+        // diagonal principal, agora o programa passa
+        // a verificar a diagonal secundaria.
+        // Para isso, utiliza linha=i e coluna=(2-i)
         cont = 0;
         for(i=0;i<LIN;i++){
             if(m[i][2-i] == jogador){
                 cont++;
             }
         }
+        // Se cada linha a contagem for igual a 3
+        // O jogador venceu pela diagonal secundaria
         if(cont == 3){
             system("clear");
             printf("\t\tJogador %c VENCEU!!\n", jogador);
@@ -129,9 +145,23 @@ void realizarJogada(char m[][COL], char jogador, int *vez){
     printf("Informe o valor de Y: ");
     scanf("%i", &y);
 
+    // Verifica se o local nos valores X e Y
+    // esta disponivel para jogada.
     if(m[x][y] == ' '){
+        // Altera o valor de m[x][y] para o
+        // valor do jogador em questao
         m[x][y] = jogador;
+        // Incrementa vez o valor de "vez" passado por
+        // referencia, para encaminhar a jogada para o
+        // outro jogador.
         *vez = *vez + 1;
+        // Verifica linhas, colunas e diagonais
+        // A funcao retorna verdadeiro se o jogador
+        // ganhou em alguma das 3 condicoes
+        // Em seguida inicializa os valores da matriz e,
+        // retorna para o valor de vez = 1, valor inicial
+        // para o  inicio das jogadas, que sao iniciadas
+        // pelo jogador "X"
         if(verificarLinhas(m, jogador)){
             inicializar(m);
             *vez = 1;
@@ -149,16 +179,21 @@ void realizarJogada(char m[][COL], char jogador, int *vez){
     }
 }
 int main(){
+    // Declaracao das variaveis
     char jogoVelha[LIN][COL];
     int opc, vez=1;
+    // Chama a funcao para inicializar a matriz do jogo da velha
     inicializar(jogoVelha);
 
     do{
         system("clear");
+        // Imprime o esta atual do jogo da velha e
+        // em seguida, mostra o menu novamente
         imprimirJogo(jogoVelha);
         opc = menu();
         switch(opc){
             case 1:
+                // Realiza a funcao MOD para alterar entre os 2 jogadores;
                 if((vez%2) == 0){
                     realizarJogada(jogoVelha, '0', &vez);
                 }else{
@@ -170,5 +205,5 @@ int main(){
             default:
                 printf("\tOpcao Invalida!\n");
         }
-    }while(opc != 2);
+    }while(opc != 2); // interrompe o loop caso o usuario escolha a opcao 2
 }
